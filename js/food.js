a1bd19d50c13a2973f0eb1a855504ea92a62d1cc
aditri0403 <69711402+aditri0403@users.noreply.github.com>
2020-11-07 11:04:40 +0530

    Add files via upload

--- js/food.js
+++ js/food.js
@@ -4,7 +4,28 @@ class Food{
         var foodStock;
         var lastFed;
     }
+    getfoodStock(){
+        var fsref = database.ref('foodStock');
+        fsref.on("value",function(data){
+            foodStock=data.val();
+        })
+    }
+    
     display(){
-        image(this.image)
+        var x=80, y=100;
+
+        imageMode(CENTER);
+        image(this.image,720,220,70,70);
+
+        if(this.foodStock!==0){
+           for(var i=0;i<this.foodStock;i++){
+               if(i%10===0){
+                   x=80;
+                   y=y+50;
+               }
+               image(this.image,x,y,50,50);
+               x=x+30;
+           }
+        }
     }
 };
--- sketch.js
+++ sketch.js
@@ -31,6 +31,20 @@ function setup() {
 function draw() {  
   background(46,139,87);
 
+  fedTime = database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed = data.val();
+  });
+  fill(255,255,254);
+  textSize(15);
+  if(lastFed>=12){
+    text("Last Feed : "+ lastFed%12 + "PM",350,30);
+  }else if(lastFed===0){
+    text("Last Feed : 12 AM",350,30);
+  }else{
+    text("Last Feed : "+ lastFed + "AM",350,30);
+  }
+
   foodObj.display();
   drawSprites();
 }
@@ -48,5 +62,18 @@ function writeStock(x){
 function readstock(data){
   foodS = data.val();
 }
-
+function feedDog(){
+  dog.addImage(vpetimg2);
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    Food : foodObj.getFoodStock(),
+    FeedTime : hour()
+  })
+}
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food : foodS
+  })
+}
 
